701b25ad82c0c41753a9f5201df14f315d88b74f
Joanna Krężołek <j.krezolek@gmail.com>
2018-03-20 22:05:11 +0100

    chat app - first commit

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+const env = process.env.NODE_ENV || 'development';
+
+console.log('NODE_ENV:', env);
+
+const plugins = [
+	new HtmlWebpackPlugin({
+		template: 'client/index.html',
+		filename: 'index.html',
+		inject: 'body',
+    })
+];
+  
+if (env === 'production') {
+	plugins.push(
+		new webpack.optimize.UglifyJsPlugin(),
+		new OptimizeJsPlugin({
+			sourceMap: false
+      	})
+    );
+}
+  
+module.exports = {
+	entry: (env !== 'production' ? [
+		'react-hot-loader/patch',
+		'webpack-dev-server/client?http://localhost:8080',
+      	'webpack/hot/only-dev-server',
+    ] : []).concat(['./client/index.js']),
+  	output: {
+		path: path.resolve(__dirname, 'public'),
+      	filename: 'bundle.js'
+  	},
+  	module: {
+    	rules: [
+			{
+				test: /\.js$/,
+				loader: "babel-loader"
+      		},
+      		{
+        		test: /\.css$/,
+        		use: [
+          			{loader: 'style-loader'},
+          			{
+            			loader: 'css-loader',
+            			options: {
+              			modules: true
+            			}
+          			}
+        		]
+      		}
+    	]
+  	}, 
+  	plugins: plugins 
+};
